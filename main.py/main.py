b677283ba9823b17c24c98089b320aad8f80bcdc
louayRm157 <elkachtoullouay@gmail.com>
2025-11-25 20:33:22 +0100

    Initial TP2 commit

---
+++ main.py/main.py
@@ -0,0 +1,11 @@
+def addition(a, b):
+    return a + b
+
+def multiplication(a, b):
+    return a * b
+
+if __name__ == "__main__":
+    a = 5
+    b = 3
+    print("Addition :", addition(a, b))
+    print("Multiplication :", multiplication(a, b))
